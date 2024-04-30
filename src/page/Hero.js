26e29038866d355637da1a8c3458bdd44003c044
Albert Andrews <03jayteee@gmail.com>
2024-04-30 17:00:26 +0000

    using splittext gsap

--- src/page/Hero.js
+++ src/page/Hero.js
@@ -1,25 +1,16 @@
 import React from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+  // gsap.registerPlugin(SplitText) 
 
 const Hero = () => {
-  useGSAP(() => {
-    gsap.to(".header-1", {
-      stagger: 0.1,
-      duration: 1,
-      repeat: -1,
-      y: 100,
-      ease: "none",
-      rotate: 360,
-      
-    });
-  });
+
 
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen">
       <div>
         <h1 className="flex header  flex-col text-[8rem] font-bold goldfish">
-          <span className="header-1 inset-1">FOLIO-</span>
+          <span className="header-1 ">FOLIO-</span>
           <span className="header-2 text-end">V1</span>
         </h1>
       </div>
